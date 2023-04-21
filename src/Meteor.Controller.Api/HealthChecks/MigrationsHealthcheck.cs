8d8307a91e2bdfa6e4818876edd208043bdc8c56
Slava Germonenko <slava.germonenko@gmail.com>
2023-04-21 15:32:03 +0200

    A small fix

--- src/Meteor.Controller.Api/HealthChecks/MigrationsHealthcheck.cs
+++ src/Meteor.Controller.Api/HealthChecks/MigrationsHealthcheck.cs
@@ -6,7 +6,7 @@ namespace Meteor.Controller.Api.HealthChecks;
 public class MigrationsHealthcheck : IHealthCheck
 {
     public MigrationsStatuses MigrationsStatus { get; set; } = MigrationsStatuses.NotStarted;
-    
+
     public Task<HealthCheckResult> CheckHealthAsync(
         HealthCheckContext context,
         CancellationToken cancellationToken = new ()
@@ -14,9 +14,9 @@ public class MigrationsHealthcheck : IHealthCheck
     {
         if (MigrationsStatus is MigrationsStatuses.Completed or MigrationsStatuses.Disabled)
         {
-            return Task.FromResult(HealthCheckResult.Healthy("Migrations are completed."));
+            return Task.FromResult(HealthCheckResult.Healthy("Migrations are completed"));
         }
-        
+
         if (MigrationsStatus is MigrationsStatuses.NotStarted or MigrationsStatuses.InProgress)
         {
             return Task.FromResult(HealthCheckResult.Degraded("Migrations are in progress"));
